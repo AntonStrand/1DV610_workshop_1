5f0a31ec77897c5eaf6cd6c1e3332ea879ba948c
MosaKasem <thescorpion119@gmail.com>
2018-09-10 10:38:38 +0200

    added class metric, unfinished

--- index.php
+++ index.php
@@ -11,15 +11,16 @@
 	  </form>
 	</body>
 	</html>
-	<?PHP
+	<?PHP require_once('Metrics.php');
 	  if(!empty($_FILES['uploaded_file']))
 	  {
-	    if (!move_uploaded_file($_FILES['uploaded_file']['tmp_name'], "uploaded.php")) {
+          // change name of path to something less generic
+          $path = "_temp/uploaded.php";
+	    if (!move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $path)) {
 	        echo "There was an error uploading the file, please try again!";
         }
-        $file = file('uploaded.php');
-        foreach($file as $line) {
-            echo $line . "<br>";
-        }
+        $file = file($path);
+        $metrics = new Metrics("Hello");
+
 	  }
 	?>
